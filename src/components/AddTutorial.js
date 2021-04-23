df92676b7c4dd80ed8670461732c9edac4ee9ee5
Kim Long <kimrlong09@gmail.com>
2021-04-23 12:20:12 -0400

    creating react components

---
+++ src/components/AddTutorial.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createTutorial } from "../actions/tutorials";
+
+const AddTutorial = () => {
+  const initialTutorialState = {
+    id: null,
+    title: "",
+    description: "",
+    published: false
+  };
+  const [tutorial, setTutorial] = useState(initialTutorialState);
+  const [submitted, setSubmitted] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    setTutorial({ ...tutorial, [name]: value });
+  };
+
+  const saveTutorial = () => {
+    const { title, description } = tutorial;
+
+    dispatch(createTutorial(title, description))
+      .then(data => {
+        setTutorial({
+          id: data.id,
+          title: data.title,
+          description: data.description,
+          published: data.published
+        });
+        setSubmitted(true);
+
+        console.log(data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  const newTutorial = () => {
+    setTutorial(initialTutorialState);
+    setSubmitted(false);
+  };
+
+  return (
+    <></>
+  );
+};
+
+export default AddTutorial;
+
+//line 48 added <> </> to fill in return and get rid of bug
